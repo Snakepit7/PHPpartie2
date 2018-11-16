0044d63cb6ee7419bf970c0584ce432087df583f
Snakepit7 <julienfollet71@gmail.com>
2018-11-16 10:02:27 +0100

    exos

---
+++ exo5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo5</title>
+  </head>
+  <body>
+    <?php
+      $gender = 'homme';
+      
+      if ($gender === 'homme') {
+          echo 'C\'est un développeur !!!';
+      } else {
+          echo 'C\'est une développeuse !!!';
+      }
+    ?>
+  </body>
+</html>
